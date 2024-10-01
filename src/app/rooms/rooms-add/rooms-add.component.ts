45bcc1513398371231bfa2a00e73f5edf0517750
namddanh <namddanh@gmail.com>
2024-10-01 16:39:04 -0400

    added room functionality, added success message alert

--- src/app/rooms/rooms-add/rooms-add.component.ts
+++ src/app/rooms/rooms-add/rooms-add.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomList } from '../rooms';
+import { RoomsService } from './../services/rooms.service';
 
 @Component({
   selector: 'hinv-rooms-add',
   templateUrl: './rooms-add.component.html',
-  styleUrls: ['./rooms-add.component.scss']
+  styleUrls: ['./rooms-add.component.scss'],
 })
 export class RoomsAddComponent implements OnInit {
+  room: RoomList = {
+    roomType: '',
+    amenities: '',
+    checkinTime: new Date(),
+    checkoutTime: new Date(),
+    photos: '',
+    price: 0,
+    rating: 0,
+  };
 
-  constructor() { }
+  successMessage: string = '';
 
-  ngOnInit(): void {
+  constructor(private roomsService: RoomsService) {}
+
+  ngOnInit(): void {}
+
+  AddRoom() {
+    this.roomsService.addRoom(this.room).subscribe((data) => {
+      this.successMessage = 'Room Added Successfully';
+    });
   }
-
 }
